83e28f5f95b91ba23a591ac85ede1706e9644b7e
bodhibrdr <julie.gumerman@gmail.com>
2020-03-05 16:12:45 -0700

    shot in the dark

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+import django_heroku
 import os
 from decouple import config
 
@@ -153,5 +153,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-import django_heroku
+#import django_heroku
 django_heroku.settings(locals())
