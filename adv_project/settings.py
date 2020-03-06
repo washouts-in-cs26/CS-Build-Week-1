0149b1278766b4ee799a8549f6d7e4e9b46840ea
Jeremy Brady <jbdigitalgeek@gmail.com>
2020-03-06 13:51:49 -0800

    settings tweak

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -102,8 +102,8 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config(
-    conn_max_age=600, ssl_require=True)
+# DATABASES['default'] = dj_database_url.config(
+#     conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -160,6 +160,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #import django_heroku
 django_heroku.settings(locals())
-
-import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
